34fcadc1b0b39dae630fa175e336b7ba0d2f4c5e
Lareta <lareta064@gmail.com>
2018-10-21 23:31:20 +0300

    edit file config

--- www/config.php
+++ www/config.php
@@ -1,12 +1,12 @@
 <?php
 //-переменные для БД
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'webdev-team04-lareta');
+define('DB_NAME', 'WD04-project-lareta');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
 //-Хост
-define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 
 //-физический путь к корневой директори скрипта
 define('ROOT', dirname(__FILE__). '/');
